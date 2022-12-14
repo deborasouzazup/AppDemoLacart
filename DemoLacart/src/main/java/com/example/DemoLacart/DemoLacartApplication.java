dd58aeb6ceceef844ae4ef494d7a934ac2f5cddb
Debora Rodrigues <debora.souza@zup.com.br>
2022-12-14 15:45:11 -0300

    Commit inicial

---
+++ DemoLacart/src/main/java/com/example/DemoLacart/DemoLacartApplication.java
@@ -0,0 +1,13 @@
+package com.example.DemoLacart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoLacartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoLacartApplication.class, args);
+	}
+
+}
